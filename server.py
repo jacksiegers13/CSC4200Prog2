8018fcdee00a47557be6691f1c06b1f644d98fe2
JackS <djsiegers42@tntech.edu>
2025-04-13 15:55:59 -0500

    created parse_header and build_response functions

--- server.py
+++ server.py
@@ -7,3 +7,16 @@ HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
 HOST = '127.0.0.1'
 PORT = 5000
 
+def parse_header(data):
+    return struct.unpack(HEADER_FORMAT, data)
+
+def build_response(header):
+    _, _, _, ack, syn, fin, payload_size = header
+    if syn == 1:
+        return "SYN received - connection initiated"
+    elif ack == 1:
+        return "ACK received - message acknowledged"
+    elif fin == 1:
+        return "FIN received - connection closing"
+    else:
+        return f"Data received - payload length: {payload_size}"
