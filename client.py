342d94083e8e6d39f14fa56e4c73802f1f23cbb3
JackS <djsiegers42@tntech.edu>
2025-04-13 15:57:01 -0500

    create_header function in client.py

--- client.py
+++ client.py
@@ -5,5 +5,7 @@ HEADER_FORMAT = '!HHIBBBH'
 SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 5000
 
-
+def create_header(source_port, dest_port, seq, ack, syn, fin, payload):
+    payload_size = len(payload)
+    return struct.pack(HEADER_FORMAT, source_port, dest_port, seq, ack, syn, fin, payload_size)
 
